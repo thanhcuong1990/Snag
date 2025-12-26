e00b162423c7b0fb93ba0a94b06df1228264e6bf
Cuong Lam <thanhcuong1990@gmail.com>
2025-12-27 00:26:20 +0700

    feat: bump Snag library to 1.0.3 and Android Gradle Plugin to 8.12.0.

--- android/snag/build.gradle.kts
+++ android/snag/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 mavenPublishing {
-    coordinates("io.github.thanhcuong1990", "snag", "1.0.2")
+    coordinates("io.github.thanhcuong1990", "snag", "1.0.3")
     publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL, automaticRelease = false)
     configure(AndroidSingleVariantLibrary())
 
