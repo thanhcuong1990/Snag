545a15a91cb7464e0d01b0d19b6acc0e578dd615
Cuong Lam <thanhcuong1990@gmail.com>
2026-01-15 00:25:32 +0700

    chore: bump version to 1.0.14

--- Snag.podspec
+++ Snag.podspec
@@ -1,7 +1,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'Snag'
-  s.version          = '1.0.13'
+  s.version          = '1.0.14'
   s.summary          = "Native cross-platform network debugger for iOS & Android - no proxies needed."
 
   s.description      = <<-DESC
--- android/snag/build.gradle.kts
+++ android/snag/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 mavenPublishing {
-    coordinates("io.github.thanhcuong1990", "snag", "1.0.13")
+    coordinates("io.github.thanhcuong1990", "snag", "1.0.14")
     publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL, automaticRelease = false)
     configure(AndroidSingleVariantLibrary())
 
