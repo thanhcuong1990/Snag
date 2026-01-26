f590914524316368c1828802c2a43b66793eda88
Cuong Lam <thanhcuong1990@gmail.com>
2026-01-26 16:00:59 +0700

    chore: bump version to 1.0.20

--- README.md
+++ README.md
@@ -75,7 +75,7 @@ Add the package URL to your project:
 #### CocoaPods
 
 ```ruby
-pod 'Snag', '~> 1.0.19'
+pod 'Snag', '~> 1.0.20'
 ```
 
 ### Usage
--- android/snag/build.gradle.kts
+++ android/snag/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 mavenPublishing {
-    coordinates("io.github.thanhcuong1990", "snag", "1.0.19")
+    coordinates("io.github.thanhcuong1990", "snag", "1.0.20")
     publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL, automaticRelease = true)
     configure(AndroidSingleVariantLibrary())
 
