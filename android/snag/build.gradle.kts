1403ea5f1e03bce9c06c29f31aa88aa3caaf3254
Cuong Lam <thanhcuong1990@gmail.com>
2026-05-25 14:19:31 +0700

    chore: bump version to v1.1.8

--- android/snag/build.gradle.kts
+++ android/snag/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 mavenPublishing {
-    coordinates("io.github.thanhcuong1990", "snag", "1.1.7")
+    coordinates("io.github.thanhcuong1990", "snag", "v1.1.8")
     publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL, automaticRelease = true)
     configure(AndroidSingleVariantLibrary())
 
